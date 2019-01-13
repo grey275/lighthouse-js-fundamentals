1ae9158fd60813d4151865131d0099ea896ef029
grey275 <pjvanderpol@gmail.com>
2019-01-13 09:40:19 -0800

    Adds conditionals exercises

---
+++ howManyHundreds.js
@@ -0,0 +1,10 @@
+function howManyHundreds (num) {
+  var floored = num - (num % 100);
+  return floored / 100;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
